645c68f5ee7739e62ad2217986665dbd5354114d
github h1karu-446 <hika.shi1227@gmail.com>
2026-03-12 23:08:28 +0900

    仕分けアルゴリズム

---
+++ 7_AdvancedSort/Partition.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> A;
+
+int partition(int p, int r){
+  int pibot, x = A[r], i, j, t;
+  for (i = p-1, j = p; j < r; j++){
+    if (A[j] <= x){
+      i++;
+      t = A[i]; A[i] = A[j]; A[j] = t;
+    }
+  }
+  pibot = ++i;
+  A[r] = A[i];
+  A[i] = x;
+  return pibot;
+}
+
+int main(){
+  int n, pibot;
+  cin >> n;
+
+  for (int i = 0; i < n; i++){
+    int t;
+    cin >> t;
+    A.push_back(t);
+  }
+
+  pibot = partition(0, n-1);
+
+  for (int i = 0; i < n; i++){
+    if (i == pibot){
+      cout << "[" << A[i] << "]";
+    } else {
+      cout << A[i];
+    }
+    cout << (i == n-1 ? "\n" : " ");
+  }
+  return 0;
+}
